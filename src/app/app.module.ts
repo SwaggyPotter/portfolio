ed9bd8df8c2cb12c09b44fd3db0b7d82713ec3f2
Tim <tim.spiele1@freenet.de>
2023-08-09 10:36:33 +0200

    about me section (stable)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MeMainComponent } from './me-main/me-main.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MeMainComponent
+    MeMainComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
