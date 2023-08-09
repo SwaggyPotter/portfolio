6c96651543da5a4040ab0e6ad4eab8e6f2df6495
Tim <tim.spiele1@freenet.de>
2023-08-09 12:01:19 +0200

    projects handy (little bug in handy version)
    scroll bar on the side since skill section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { MeMainComponent } from './me-main/me-main.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MySkillsComponent } from './my-skills/my-skills.component';
     HeaderComponent,
     MeMainComponent,
     AboutMeComponent,
-    MySkillsComponent
+    MySkillsComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
