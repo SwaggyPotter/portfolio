17ccc1d601753c13e22d4bb21aca33967dbd6581
Tim <tim.spiele1@freenet.de>
2023-08-16 00:28:39 +0200

    set up email contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -14,6 +14,7 @@ import { FooterComponent } from './footer/footer.component';
 import { UpButtonComponent } from './up-button/up-button.component';
 import { ImpressumComponent } from './impressum/impressum.component';
 import { MySiteComponent } from './my-site/my-site.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,9 @@ import { MySiteComponent } from './my-site/my-site.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import emailjs from '@emailjs/browser';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact-form',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent {
+  form: FormGroup = this.fb.group({
+    from_name: "",
+    from_email: "",
+    message: "",
+  })
 
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  async send() {
+    emailjs.init('HXRHgLXNMAqXvgQza')
+    let response = await emailjs.send("service_gjpw7iv", "template_7extjmi", {
+      from_name: this.form.value.from_name,
+      from_email: this.form.value.from_email,
+      message: this.form.value.message,
+    });
+
+    alert('Message send')
+    this.form.reset()
+  }
 }
