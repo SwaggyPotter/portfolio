207d422aa4ad6c51045c9492f9836de706abc11a
Tim <tim.spiele1@freenet.de>
2023-08-17 01:00:11 +0200

    vehindert das contact formular einfach versendet werden kann ohne nötige daten

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -3,12 +3,12 @@ import emailjs from '@emailjs/browser';
 import { FormGroup, FormControl } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 
-
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
+
 export class ContactFormComponent {
   form: FormGroup = this.fb.group({
     from_name: "",
@@ -21,14 +21,19 @@ export class ContactFormComponent {
   }
 
   async send() {
-    emailjs.init('HXRHgLXNMAqXvgQza')
-    let response = await emailjs.send("service_gjpw7iv", "template_7extjmi", {
-      from_name: this.form.value.from_name,
-      from_email: this.form.value.from_email,
-      message: this.form.value.message,
-    });
+    if (this.form.value.from_name.length != 0 && this.form.value.from_email.length != 0 && this.form.value.message.length != 0) {
+      emailjs.init('HXRHgLXNMAqXvgQza')
+      let response = await emailjs.send("service_gjpw7iv", "template_7extjmi", {
+        from_name: this.form.value.from_name,
+        from_email: this.form.value.from_email,
+        message: this.form.value.message,
+      });
 
-    alert('Message send')
-    this.form.reset()
+      alert('Message send')
+      this.form.reset()
+    }
+    else {
+      alert('Nachricht konnte nicht zugestellt werden')
+    }
   }
 }
